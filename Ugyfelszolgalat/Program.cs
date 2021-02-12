4635529d7fad03dba2f338406d30d2063111da9a
Reka Ehreth <ehreth.reka@gmail.com>
2021-02-13 00:08:07 +0100

    Add project files.

---
+++ Ugyfelszolgalat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace telefon
+{
+    class Program
+    {
+        /// <summary>
+        /// Changes the time given as parameter to secunds. 
+        /// </summary>
+        /// <param name="o"> hours </param>
+        /// <param name="p"> minutes </param>
+        /// <param name="mp"> secunds </param>
+        /// <returns> The parameter in secunds </returns>
+        static int Mpbe (int o, int p, int mp)
+        {
+            return mp + p * 60 + o * 60 * 60;
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
