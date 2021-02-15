97a3350b3744d6865b20cd93ee265ac58b138f1a
Reka Ehreth <ehreth.reka@gmail.com>
2021-02-15 17:49:09 +0100

    4. feladat első fele - saját típus létrehozása List - tömb szteroidokon. Public - mindenki közkuvája, bárki hozzáfér, és láthatja a tagjait.

--- Ugyfelszolgalat/Program.cs
+++ Ugyfelszolgalat/Program.cs
@@ -3,6 +3,10 @@ using System.IO;
 
 namespace telefon
 {
+    class CallData
+    {
+        public int startH, startMin, startSec, endH, endMin, endSec;
+    }
     class Program
     {
         /// <summary>
@@ -21,6 +25,9 @@ namespace telefon
             int[] callsPerHours = new int[24];
             StreamReader file = new StreamReader("hivas.txt");
             bool readLineSuccesful = true;
+            int maxCallLength = 0;
+            int maxCallIndex = 0;
+            int callIndex = 0;
             while (readLineSuccesful)
             {
                 string line = file.ReadLine();
@@ -29,18 +36,28 @@ namespace telefon
                     readLineSuccesful = false;
                     break;
                 }
-                int startH, startMin, startSec, endH, endMin, endSec; 
+                CallData callData = new CallData();
                 string[] lineData = line.Split(' ');
-                startH = Int32.Parse(lineData[0]);
-                startMin = Int32.Parse(lineData[1]);
-                startSec = Int32.Parse(lineData[2]);
-                endH = Int32.Parse(lineData[3]);
-                endMin = Int32.Parse(lineData[4]);
-                endSec = Int32.Parse(lineData[5]);
+                callData.startH = Int32.Parse(lineData[0]);
+                callData.startMin = Int32.Parse(lineData[1]);
+                callData.startSec = Int32.Parse(lineData[2]);
+                callData.endH = Int32.Parse(lineData[3]);
+                callData.endMin = Int32.Parse(lineData[4]);
+                callData.endSec = Int32.Parse(lineData[5]);
 
-                callsPerHours[startH] += 1;
+                // 3. feladat
+                callsPerHours[callData.startH] += 1;
+                // 4. feladat
+                int callLength = Mpbe(callData.endH, callData.endMin, callData.endSec) - Mpbe(callData.startH, callData.startMin, callData.startSec);
+                if (maxCallLength < callLength)
+                {
+                    maxCallLength = callLength;
+                    maxCallIndex = callIndex;
+                }
+                callIndex = callIndex + 1;
             }
             #region Harmadik feladat
+            // Megszámlálás
             Console.WriteLine("3. feladat");
             for (int currentHour = 0; currentHour < callsPerHours.Length; currentHour++)
             {
@@ -49,6 +66,20 @@ namespace telefon
                     Console.WriteLine($"{currentHour} ora {callsPerHours[currentHour]} hivas");
                 }
             }
+            Console.Write("\n");
+            #endregion
+            #region Negyedik feladat
+            // Maximum keresés
+            Console.WriteLine("4. feladat");
+            Console.WriteLine($"A leghosszabb ideig vonalban levo hivo {maxCallIndex + 1}. sorban szerepel, a hivas hossza: {maxCallLength} masodperc.");
+            Console.Write("\n");
+            #endregion
+            #region Otodik feladat
+            // Maximum keresés
+            Console.WriteLine("5. feladat");
+            Console.Write("Adjon meg egy idopontot 8 es 12 kozott! (ora perc masodperc) ");
+            Console.ReadLine();
+            Console.Write("\n");
             #endregion
         }
     }
