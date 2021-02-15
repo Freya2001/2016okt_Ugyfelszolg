8c6f922d9e733913f5651f0bab709ed227ba4a27
Reka Ehreth <ehreth.reka@gmail.com>
2021-02-15 14:41:21 +0100

    1-3 feladat

--- Ugyfelszolgalat/Program.cs
+++ Ugyfelszolgalat/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace telefon
 {
@@ -17,7 +18,38 @@ namespace telefon
         }
         static void Main(string[] args)
         {
-            
+            int[] callsPerHours = new int[24];
+            StreamReader file = new StreamReader("hivas.txt");
+            bool readLineSuccesful = true;
+            while (readLineSuccesful)
+            {
+                string line = file.ReadLine();
+                if( line == null)
+                {
+                    readLineSuccesful = false;
+                    break;
+                }
+                int startH, startMin, startSec, endH, endMin, endSec; 
+                string[] lineData = line.Split(' ');
+                startH = Int32.Parse(lineData[0]);
+                startMin = Int32.Parse(lineData[1]);
+                startSec = Int32.Parse(lineData[2]);
+                endH = Int32.Parse(lineData[3]);
+                endMin = Int32.Parse(lineData[4]);
+                endSec = Int32.Parse(lineData[5]);
+
+                callsPerHours[startH] += 1;
+            }
+            #region Harmadik feladat
+            Console.WriteLine("3. feladat");
+            for (int currentHour = 0; currentHour < callsPerHours.Length; currentHour++)
+            {
+                if(callsPerHours[currentHour] != 0)
+                {
+                    Console.WriteLine($"{currentHour} ora {callsPerHours[currentHour]} hivas");
+                }
+            }
+            #endregion
         }
     }
 }
